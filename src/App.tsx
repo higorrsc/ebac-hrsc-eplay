d35d288bf10fcbcbce5dc3508e8df8fa836577ed
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-06-21 19:34:28 -0300

    conteúdo aula 08 módulo 35

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { GlobalCss } from './styles'
 import Rotas from './routes'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
       </div>
       <Rotas />
+      <Footer />
     </BrowserRouter>
   )
 }

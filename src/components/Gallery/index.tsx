3bdd396e721bf3fadf95cfaf2684bf4c940078c1
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-06-27 21:16:00 -0300

    conteúdo aula 06 módulo 36

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import Section from '../Section'
 
 import { Items, Item, Action, Modal, ModalContent } from './styles'
@@ -8,7 +10,7 @@ import play from '../../assets/images/play.png'
 import zoom from '../../assets/images/zoom.png'
 import fechar from '../../assets/images/fechar.png'
 
-type GalleryItem = {
+interface GalleryItem {
   type: 'image' | 'video'
   url: string
 }
@@ -33,7 +35,17 @@ type Props = {
   name: string
 }
 
+interface ModalState extends GalleryItem {
+  isVisible: boolean
+}
+
 const Gallery = ({ defaultCover, name }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
   const getMediaCover = (item: GalleryItem) => {
     if (item.type === 'image') return item.url
     return defaultCover
@@ -43,12 +55,29 @@ const Gallery = ({ defaultCover, name }: Props) => {
     return play
   }
 
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: ''
+    })
+  }
+
   return (
     <>
       <Section title="Galeria" background="black">
         <Items>
           {mock.map((media, index) => (
-            <Item key={index}>
+            <Item
+              key={index}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
               <img
                 src={getMediaCover(media)}
                 alt={`Media ${index + 1} de ${name}`}
@@ -63,15 +92,23 @@ const Gallery = ({ defaultCover, name }: Props) => {
           ))}
         </Items>
       </Section>
-      <Modal>
+      <Modal className={modal.isVisible ? 'visible' : ''}>
         <ModalContent className="container">
           <header>
             <h4>{name}</h4>
-            <img src={fechar} alt="Ícone de fechar" />
+            <img
+              src={fechar}
+              alt="Ícone de fechar"
+              onClick={() => closeModal()}
+            />
           </header>
-          <img src={spiderman} alt="" />
+          {modal.type === 'image' ? (
+            <img src={modal.url} alt="" />
+          ) : (
+            <iframe src={modal.url} frameBorder={0} />
+          )}
         </ModalContent>
-        <div className="overlay"></div>
+        <div className="overlay" onClick={() => closeModal()}></div>
       </Modal>
     </>
   )
